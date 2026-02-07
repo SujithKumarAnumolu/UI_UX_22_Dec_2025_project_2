35d5bd7f2c2ad2dfe356578b72e25a07c167a263
SujithKumarAnumolu <sujithkumaranumolu23@gmail.com>
2026-02-07 08:48:43 -0500

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('UI_UX_22_Dec_2025_project_2');
+}
